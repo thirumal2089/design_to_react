9de6b41f24c31a8119dd8fee37800a1f4a29efa9
thirumal2089 <tgthirumalesh4238@gmail.com>
2024-02-29 12:17:34 +0530

    first commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,20 +1,45 @@
-import pic  from './picture.png' ;
-import './index.css' ;
+import pic from './picture.png';
+import './index.css';
+import React, { useState, useEffect } from 'react';
 
-const Home=() => {
-    return(
-     <div className="container">
-        <div className='sub-container1'>
-            <h1 className='title'>Free Trial</h1>
-            <p className='para'>Creating something beautiful with coding is hugely rewarding! It takes discipline and hours of effort, but the sense of accomplishment at the end is worth it.</p>
+const Home = () => {
+    const [paraContent, setParaContent] = useState("Creating something beautiful with coding is hugely rewarding! It takes discipline and hours of effort, but the sense of accomplishment at the end is worth it.");
+    const [buttonContent,setButtonContent] = useState("Try Now");
+    useEffect(() => {
+        function handleResize() {
+            if (window.innerWidth <= 464) {
+                setButtonContent("Try Now");
+                setParaContent("Subscribe to HackerKID’s YouTube channel to never miss the coding tricks, hacks, parental guides, tutorials, & other exclusive content to make your coding journey more fun, enriching and effective!");
+            }
+            else if (window.innerWidth <= 680 ) {
+                setButtonContent("Play Now");
+                setParaContent("Subscribe to HackerKID’s YouTube channel to never miss the coding tricks, hacks, parental guides, tutorials, & other exclusive content to make your coding journey more fun, enriching and effective!");
+            }
+            else {
+                setButtonContent("Try Now");
+                setParaContent("Creating something beautiful with coding is hugely rewarding! It takes discipline and hours of effort, but the sense of accomplishment at the end is worth it.");
+            }
+        }
 
-            <button className='button'>Try Now</button>
+        window.addEventListener('resize', handleResize);
+
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return (
+        <div className="container">
+            <div className='sub-container1'>
+                <h1 className='title'>Free Trial</h1>
+                <p className='para'>{paraContent}</p>
+                <button className='button'>{buttonContent}</button>
+            </div>
+
+            <div className='sub-container2'>
+                <img src={pic} className="picture" alt="example" />
+            </div>
         </div>
-        
-        <div className='sub-container2'>
-            <img src={pic} className="picture" />
-        </div>
-     </div>
     );
 };
 export default Home;
